071de07046e927aa92cc2d01e882f55663410aec
RunItBack1127 <thewikiscience@gmail.com>
2022-09-06 19:27:43 -0400

    Implementing v-model functionality between child input and parent

--- src/components/BasicFormInput.vue
+++ src/components/BasicFormInput.vue
@@ -1,11 +1,53 @@
 <template>
-
+    <section :class="disabled === true ? 'disabled' : ''">
+        <label>{{ title }}</label>
+        <input
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target?.value)"
+            :placeholder="placeholder"
+            :disabled="disabled === true"
+            type="text" />
+    </section>
 </template>
 
-<script setup lang="ts">
-
+<script lang="ts">
+export default {
+    props: ['title', 'placeholder', 'disabled', 'modelValue']
+}
 </script>
 
 <style lang="scss" scoped>
+section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100%;
 
+    label {
+        font-size: 1.3rem;
+        font-weight: 500;
+        margin-bottom: 20px;
+    }
+
+    input {
+        width: 100%;
+        font-size: 1.1rem;
+        padding: 20px;
+        border-radius: 15px;
+        border: 1px solid rgb(0, 0, 0, 0.5);
+    }
+
+    input::placeholder {
+        opacity: 0.35;
+    }
+}
+
+section:not(:last-child) {
+    margin-bottom: 40px;
+}
+
+section.disabled {
+    opacity: 0.25;
+}
 </style>
--- src/views/CoverLetterFormView.vue
+++ src/views/CoverLetterFormView.vue
@@ -1,25 +1,55 @@
 <template>
     <form>
-        <BasicFormInput title="Name of Role" />
-        <BasicFormInput title="Company Name" />
+        <BasicFormInput title="Name of Role" placeholder="Software Engineer" v-model="nameOfRole" />
+        <BasicFormInput title="Company Name" placeholder="Cruise, LLC" v-model="companyName" />
         <div class="customRecruiterContainer">
             <label for="customRecruiterCheckbox">Use custom recruiter name?</label>
-            <input :checked="useCustomRecruiterName" type="checkbox" />
+            <input type="checkbox" v-model="useCustomRecruiterName" />
         </div>
-        <BasicFormInput title="Recruiter Name" />
+        <BasicFormInput
+            title="Recruiter Name"
+            placeholder="Corporate Recruiter"
+            v-model="recruiterName"
+            :disabled="!useCustomRecruiterName" />
+        <SubmitClearMenu />
     </form>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import BasicFormInput from '@/components/BasicFormInput.vue';
-import { defineComponent, ref } from 'vue';
+import SubmitClearMenu from '../components/SubmitClearMenu.vue';
+import { defineComponent } from 'vue';
 
-const nameOfRole = ref("");
-const companyName = ref("");
-const recruiterName = ref("");
-const useCustomRecruiterName = ref(false);
+export default defineComponent({
+    components: {
+    BasicFormInput,
+    SubmitClearMenu
+},
+    data() {
+        return {
+            nameOfRole: '',
+            companyName: '',
+            recruiterName: '',
+            useCustomRecruiterName: false
+        }
+    }
+})
 </script>
 
 <style lang="scss" scoped>
+form {
+    .customRecruiterContainer {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        width: 100%;
 
+        label {
+            font-size: 1.1rem;
+            font-weight: 500;
+            margin-right: 15px;
+            opacity: 0.9;
+        }
+    }
+}
 </style>
