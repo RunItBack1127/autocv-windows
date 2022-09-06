19fabef60c9c82e737eea003e6176f07630c1776
RunItBack1127 <thewikiscience@gmail.com>
2022-09-06 18:06:50 -0400

    Implementing start of resume form view

--- src/views/CoverLetterFormView.vue
+++ src/views/CoverLetterFormView.vue
@@ -1,16 +1,23 @@
 <template>
-    <SiteHeader />
+    <form>
+        <BasicFormInput title="Name of Role" />
+        <BasicFormInput title="Company Name" />
+        <div class="customRecruiterContainer">
+            <label for="customRecruiterCheckbox">Use custom recruiter name?</label>
+            <input :checked="useCustomRecruiterName" type="checkbox" />
+        </div>
+        <BasicFormInput title="Recruiter Name" />
+    </form>
 </template>
 
-<script lang="ts">
-import SiteHeader from '@/components/SiteHeader.vue';
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import BasicFormInput from '@/components/BasicFormInput.vue';
+import { defineComponent, ref } from 'vue';
 
-export default defineComponent({
-    components: {
-        SiteHeader
-    }
-});
+const nameOfRole = ref("");
+const companyName = ref("");
+const recruiterName = ref("");
+const useCustomRecruiterName = ref(false);
 </script>
 
 <style lang="scss" scoped>
--- src/views/ResumeFormView.vue
+++ src/views/ResumeFormView.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Trying This</h1>
+    
 </template>
 
 <script lang="ts">
