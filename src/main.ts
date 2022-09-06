841a72754d82c6686fce0650b6b2c9f0d8f181dd
RunItBack1127 <thewikiscience@gmail.com>
2022-09-06 04:30:00 -0400

    Appending views

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 
+import ResumeFormView from '@/views/ResumeFormView.vue';
+import CoverLetterFormView from '@/views/CoverLetterFormView.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
