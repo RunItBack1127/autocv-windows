2957ce99c0c6eacfc211924cf5a5fbfa1b4c13df
RunItBack1127 <thewikiscience@gmail.com>
2022-09-06 04:28:29 -0400

    Resolving alias for src directory

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,29 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: to => {
+                return {
+                    path: '/resume'
+                }
+            }
+        },
+        {
+            path: '/resume',
+            name: 'Resume',
+            component: ResumeFormView
+        },
+        {
+            path: '/cv',
+            name: 'Cover Letter',
+            component: CoverLetterFormView
+        }
+    ]
+});
+
+createApp(App).use(router).mount('#app');
