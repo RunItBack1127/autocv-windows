7d254867cdcef1fd908fd38347665a569e2010cd
RunItBack1127 <thewikiscience@gmail.com>
2022-09-06 04:36:46 -0400

    Appending view boilerplate

---
+++ src/views/ResumeFormView.vue
@@ -0,0 +1,18 @@
+<template>
+    <SiteHeader />
+</template>
+
+<script lang="ts">
+import SiteHeader from '@/components/SiteHeader.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    components: {
+        SiteHeader
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
