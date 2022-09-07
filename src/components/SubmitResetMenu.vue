504eae365a7e991ba2b7d11e6fb74d1b2de06cdf
RunItBack1127 <thewikiscience@gmail.com>
2022-09-06 20:06:54 -0400

    Renaming submit and clear menu

---
+++ src/components/SubmitResetMenu.vue
@@ -0,0 +1,54 @@
+<template>
+    <footer>
+        <input type="submit" value="Submit" />
+        <input type="button" value="Reset" @click="$emit('resetFormFields')" />
+    </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    methods: {
+        onClear() {
+            this.$emit("resetFormFields");
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+footer {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    input {
+        padding: 20px 50px;
+        font-size: 1.1rem;
+        border-radius: 10px;
+        border: 1px solid #000;
+        text-transform: uppercase;
+        letter-spacing: 0.25rem;
+        cursor: pointer;
+    }
+
+    input[type="submit"] {
+        background-color: #000;
+        color: #fff;
+        opacity: 0.8;
+    }
+
+    input[type="submit"]:hover {
+        opacity: 1.0;
+    }
+
+    input[type="button"] {
+        background: transparent;
+    }
+
+    input:not(:last-child) {
+        margin-right: 30px;
+    }
+}
+</style>
